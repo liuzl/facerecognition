d5573d6959bfae1b6015cdc748ecf11152969f0f
liuzhanliang <zhanliangliu@qq.com>
2018-04-12 15:49:23 +0800

    shell client

---
+++ tests/check.sh
@@ -0,0 +1,13 @@
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <img1> <img2>"
+    exit 1;
+fi
+
+img1=`base64 -i $1 | tr -d '\n' | xxd -plain | sed 's/\(..\)/%\1/g' `
+img2=`base64 -i $2 | tr -d '\n' | xxd -plain | sed 's/\(..\)/%\1/g' `
+
+echo "img1=$img1&img2=$img2" > postdata
+
+curl -d @postdata -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:5000/api/v1/facecompare
+rm postdata
+echo ""
