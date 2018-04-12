bc2e9939a1a7ff7efa31bc54dd4acf31fa5a6566
liuzhanliang <zhanliangliu@qq.com>
2018-04-12 11:40:08 +0800

    add dnn face feature extractor

---
+++ dnn_face_feature.h
@@ -0,0 +1,139 @@
+#include <dlib/dnn.h>
+#include <dlib/clustering.h>
+#include <dlib/string.h>
+#include <dlib/image_io.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+
+using namespace dlib;
+using namespace std;
+
+// ----------------------------------------------------------------------------------------
+
+// The next bit of code defines a ResNet network.  It's basically copied
+// and pasted from the dnn_imagenet_ex.cpp example, except we replaced the loss
+// layer with loss_metric and made the network somewhat smaller.  Go read the introductory
+// dlib DNN examples to learn what all this stuff means.
+//
+// Also, the dnn_metric_learning_on_images_ex.cpp example shows how to train this network.
+// The dlib_face_recognition_resnet_model_v1 model used by this example was trained using
+// essentially the code shown in dnn_metric_learning_on_images_ex.cpp except the
+// mini-batches were made larger (35x15 instead of 5x5), the iterations without progress
+// was set to 10000, and the training dataset consisted of about 3 million images instead of
+// 55.  Also, the input layer was locked to images of size 150.
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
+
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual_down = add_prev2<avg_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
+
+template <int N, template <typename> class BN, int stride, typename SUBNET> 
+using block  = BN<con<N,3,3,1,1,relu<BN<con<N,3,3,stride,stride,SUBNET>>>>>;
+
+template <int N, typename SUBNET> using ares      = relu<residual<block,N,affine,SUBNET>>;
+template <int N, typename SUBNET> using ares_down = relu<residual_down<block,N,affine,SUBNET>>;
+
+template <typename SUBNET> using alevel0 = ares_down<256,SUBNET>;
+template <typename SUBNET> using alevel1 = ares<256,ares<256,ares_down<256,SUBNET>>>;
+template <typename SUBNET> using alevel2 = ares<128,ares<128,ares_down<128,SUBNET>>>;
+template <typename SUBNET> using alevel3 = ares<64,ares<64,ares<64,ares_down<64,SUBNET>>>>;
+template <typename SUBNET> using alevel4 = ares<32,ares<32,ares<32,SUBNET>>>;
+
+using anet_type = loss_metric<fc_no_bias<128,avg_pool_everything<
+                            alevel0<
+                            alevel1<
+                            alevel2<
+                            alevel3<
+                            alevel4<
+                            max_pool<3,3,2,2,relu<affine<con<32,7,7,2,2,
+                            input_rgb_image_sized<150>
+                            >>>>>>>>>>>>;
+
+// ----------------------------------------------------------------------------------------
+
+std::vector<matrix<rgb_pixel>> jitter_image(
+    const matrix<rgb_pixel>& img
+);
+
+class face_feature
+{
+public:
+    face_feature(const string& model_dir)
+    {
+        detector = get_frontal_face_detector();
+        deserialize(model_dir + directory::get_separator() +
+                "shape_predictor_5_face_landmarks.dat") >> sp;
+        deserialize(model_dir + directory::get_separator() +
+                "dlib_face_recognition_resnet_model_v1.dat") >> net;
+    }
+
+    bool extract(const string& file_name, matrix<float,0,1>& face_descriptor, string& msg);
+private:
+    frontal_face_detector detector;
+    shape_predictor sp;
+    anet_type net;
+};
+
+// ----------------------------------------------------------------------------------------
+
+bool face_feature::extract(const string& file_name,
+        matrix<float,0,1>& face_descriptor, string& msg)
+{
+    matrix<rgb_pixel> img;
+    load_image(img, file_name);
+
+    ostringstream oss;
+
+    std::vector<matrix<rgb_pixel>> faces;
+    for (auto face : detector(img))
+    {
+        auto shape = sp(img, face);
+        matrix<rgb_pixel> face_chip;
+        extract_image_chip(img, get_face_chip_details(shape,150,0.25), face_chip);
+        faces.push_back(move(face_chip));
+    }
+
+    if (faces.size() != 1)
+    {
+        oss << "should only contain one face in image, " << faces.size() << " faces found!";
+        msg = oss.str();
+        return false;
+    }
+
+    // This call asks the DNN to convert each face image in faces into a 128D vector.
+    // In this 128D vector space, images from the same person will be close to each other
+    // but vectors from different people will be far apart.  So we can use these vectors to
+    // identify if a pair of images are from the same person or from different people.  
+    std::vector<matrix<float,0,1>> face_descriptors = net(faces);
+   
+    // It should also be noted that face recognition accuracy can be improved if jittering
+    // is used when creating face descriptors.  In particular, to get 99.38% on the LFW
+    // benchmark you need to use the jitter_image() routine to compute the descriptors,
+    // like so:
+    face_descriptor = mean(mat(net(jitter_image(faces[0]))));
+    //cout << "jittered face descriptor for one face: " << trans(face_descriptor) << endl;
+    // If you use the model without jittering, as we did when clustering the bald guys, it
+    // gets an accuracy of 99.13% on the LFW benchmark.  So jittering makes the whole
+    // procedure a little more accurate but makes face descriptor calculation slower.
+    return true;
+}
+
+// ----------------------------------------------------------------------------------------
+
+std::vector<matrix<rgb_pixel>> jitter_image(
+    const matrix<rgb_pixel>& img
+)
+{
+    // All this function does is make 100 copies of img, all slightly jittered by being
+    // zoomed, rotated, and translated a little bit differently. They are also randomly
+    // mirrored left to right.
+    thread_local dlib::rand rnd;
+
+    std::vector<matrix<rgb_pixel>> crops; 
+    for (int i = 0; i < 100; ++i)
+        crops.push_back(jitter_image(img,rnd));
+
+    return crops;
+}
+
+// ----------------------------------------------------------------------------------------
+
--- face_compare_server.cpp
+++ face_compare_server.cpp
@@ -15,6 +15,11 @@
 #include <dlib/server.h>
 #include <dlib/base64.h>
 #include <dlib/md5.h>
+#include <dlib/dir_nav.h>
+#include <dlib/cmd_line_parser.h>
+#include <dlib/misc_api.h>
+
+#include "dnn_face_feature.h"
 
 using namespace dlib;
 using namespace std;
@@ -47,30 +52,95 @@ class web_server : public server_http
         {
             string img1 = c(incoming.queries["img1"]);
             string img2 = c(incoming.queries["img2"]);
-            string name1 = md5(img1);
-            string name2 = md5(img2);
+            string name1 = dir_ + directory::get_separator() + md5(img1);
+            string name2 = dir_ + directory::get_separator() + md5(img2);
             sout << "md5(img1)=" << name1 << endl;
             sout << "md5(img2)=" << name2 << endl;
-            write_file(name1, img1);
-            write_file(name2, img2);
+            if (!file_exists(name1))
+            {
+                write_file(name1, img1);
+            }
+            if (!file_exists(name2))
+            {
+                write_file(name2, img2);
+            }
+
+            matrix<float,0,1> m1, m2;
+            string msg;
+            feature_util_->extract(name1, m1, msg);
+            sout << trans(m1) << endl;
+            sout << msg << endl;
         }
         else
         {
             sout << incoming.path << endl;
         }
-        sout << "incoming.body: " << incoming.body << endl;
+        //sout << "incoming.body: " << incoming.body << endl;
        
         return sout.str();
     }
 
+public:
+    web_server(string& dir, string& model_dir)
+    {
+        dir_ = dir;
+        if (!file_exists(dir_))
+        {
+            create_directory(dir_);
+        }
+        feature_util_ = new face_feature(model_dir);
+    }
+
+private:
+    string dir_;
+    face_feature* feature_util_;
 };
 
-int main()
+int main(int argc, char** argv)
 {
     try
     {
+        command_line_parser parser;
+        parser.add_option("dir","dir for saving image files.",1);
+        parser.add_option("model","dir for dnn face model files.",1);
+        parser.add_option("h","Display this help message.");
+
+        parser.parse(argc,argv);
+
+        const char* one_time_opts[] = {"dir", "model"};
+        parser.check_one_time_options(one_time_opts);
+
+        if (parser.option("h"))
+        {
+            cout << "Usage: " << argv[0] << " --dir dir_path\n";
+            parser.print_options();
+            return 0;
+        }
+
+        string dir, model_dir;
+        if (parser.option("dir"))
+        {
+            dir = parser.option("dir").argument();
+        }
+        else
+        {
+            cout << "Error in command line:\n   You must specify an dir name.\n";
+            cout << "\nTry the -h option for more information." << endl;
+            return 0;
+        }
+        if (parser.option("model"))
+        {
+            model_dir = parser.option("model").argument();
+        }
+        else
+        {
+            cout << "Error in command line:\n   You must specify an model dir.\n";
+            cout << "\nTry the -h option for more information." << endl;
+            return 0;
+        }
+
         // create an instance of our web server
-        web_server our_web_server;
+        web_server our_web_server(dir, model_dir);
 
         // make it listen on port 5000
         our_web_server.set_listening_port(5000);
